42c8634a7e196bc878e5d06d365fde4fc5e164d3
Ian Babrou <ibobrik@gmail.com>
2014-12-16 22:15:41 +0300

    fixed index setting for beta3

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ KIBANA_ES_URL=${KIBANA_ES_URL:-http://localhost:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
 
 sed -i "s;^elasticsearch:.*;elasticsearch: ${KIBANA_ES_URL};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
-sed -i "s;^kibanaIndex:.*;kibanaIndex: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
+sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 
 exec /opt/kibana-${KIBANA_VERSION}/bin/kibana
