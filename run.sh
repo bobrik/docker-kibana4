fc86d3fec3289ec8571980f7d09ca59bbc8e4c2e
Stefan Thies <s.thies@bigdata-analyst.de>
2015-03-29 12:41:59 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ set -e
 
 KIBANA_ES_URL=${KIBANA_ES_URL:-http://localhost:9200}
 KIBANA_INDEX=${KIBANA_INDEX:-kibana-int}
+KIBANA_USER=${KIBANA_USER:-kibana}
+KIBANA_PASSWORD=${KIBANA_PASSWORD:-secret}
+echo $KIBANA_PASSWORD > htpasswd /opt/kibana-${KIBANA_VERSION}/src/.htpasswd $KIBANA_USER
 
 sed -i "s;^elasticsearch_url:.*;elasticsearch_url: ${KIBANA_ES_URL};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
 sed -i "s;^kibana_index:.*;kibana_index: ${KIBANA_INDEX};" "/opt/kibana-${KIBANA_VERSION}/config/kibana.yml"
